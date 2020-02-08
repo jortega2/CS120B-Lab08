11b5c50513aa8024c0a7d0de11d375edaa9f255c
jortega2 <alexortega1932@hotmail.com>
2020-02-08 01:17:51 -0800

    completed part 3

---
+++ turnin/jorte057_lab8_part3.c
@@ -0,0 +1,47 @@
+/*	Author: jorte057
+ *  Partner(s) Name: Duke Pham dpham073@ucr.edu
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+unsigned short MAX = 0xD8;
+
+void ADC_init(){
+	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+	//ADEN: setting this bit enables analog to digital conversion
+	//ADSC: setting this bit starts the first conversion.
+	//ADATE: setting this bit enables auto-triggering. sine we are 
+	//	in free running mode, a new conversion will trigger whenever
+	//	the previous conversion completes
+}
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+	DDRD = 0xFF; PORTD = 0x00;
+	ADC_init();
+	
+    /* Insert your solution below */
+    while (1) {
+	unsigned short x = ADC;
+	unsigned char B;
+	unsigned char D;
+	if (x >= MAX/2){
+		B = (char)x;
+		D = (char)(x>>8);
+	} else {
+		B = D = 0;
+	}
+	PORTB = B;
+	PORTD = D;	
+    }
+    
+}
